7e5f79266d2e74fd51dde12032e52219a6cf37a5
John Daniel Marañan <johndanielmaranan@gmail.com>
2025-08-13 10:53:11 +0800

    # Please enter the commit message for your changes. Lines starting     # with '#' will be ignored, and an empty message aborts the commit.     #     # On branch master     # Changes to be committed:     #	deleted:    app/Views/home.php     # Resolved merge conflicts and updated navbar

--- app/Views/home.php
+++
@@ -1,5 +0,0 @@
-<?= $this->extend('template')?>
-<div class="text-center">
-<h1 class="text-primary">Welcome to My CodeIgniter App!</h1>
-<p>This is the home page content injected into the template.</p>
-</div>
